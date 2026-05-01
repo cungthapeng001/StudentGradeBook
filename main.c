d31971dfe673b45430518a235a9a25283a1526fb
Cung Thapeng <cungthapeng@example.com>
2026-05-01 22:07:56 +0530

    main

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 void showMenu() {
     printf("\n====================================\n");
-    printf(" \t\t\tSTUDENT RESULT SYSTEM\n");
+    printf(" \tSTUDENT RESULT SYSTEM\n");
     printf("====================================\n");
     printf("1. Add New Student Data\n");
     printf("2. Search Result by Name\n");
@@ -29,50 +29,80 @@ int main() {
     Student studentList[MAX];
     char subjectName[MAX][50];
     int choice;
+    int authChoice;
+    bool isAuthenticated = false;
 
-    if (!login()){
-        return 0;
+    while (1) {
+        isAuthenticated = false; // Reset authentication status for each loop
+        
+        do {
+            printf("\n====================================\n");
+            printf(" \tAUTHENTICATION MENU\n");
+            printf("====================================\n");
+            printf("1. Login\n");
+            printf("2. Register\n");
+            printf("3. Exit\n");
+            printf("Enter your choice: ");
+            
+            if (scanf("%d", &authChoice) != 1) {
+                getchar(); // clear input
+                continue;
+            }
+            getchar(); // consume newline
+
+            switch(authChoice) {
+                case 1:
+                    isAuthenticated = login();
+                    break;
+                case 2:
+                    registerUser();
+                    break;
+                case 3:
+                    printf("Exiting program...\n");
+                    return 0;
+                default:
+                    printf("Invalid choice! Try again.\n");
+            }
+        } while (!isAuthenticated);
+
+        loadData(&students, &subjects, studentList, subjectName);
+        printf("\t\t\tWelcome! Student Grade Calculation:\n");
+        setupSubjects(&subjects, subjectName);
+
+        do {
+            showMenu();
+            if (scanf("%d", &choice) != 1) break; 
+            getchar();
+
+            switch(choice) {
+                case 1:
+                    addStudentData(&students, subjects, studentList, subjectName);
+                    break;
+                case 2:
+                    searchByName(students, studentList);
+                    break;
+                case 3:
+                    searchByRoll(students, studentList);
+                    break;
+                case 4: 
+                    showClassTopper(students, studentList);
+                    break;
+                case 5:
+                    printResultTable(students, subjects, studentList, subjectName);
+                    break;
+                case 6:
+                    deleteStudentData(&students, studentList);
+                    break;
+                case 7:
+                   saveData(students, subjects, studentList, subjectName);
+                   printf("Logging out... returning to authentication menu.\n");
+                   writeLog("User logged out");
+                    break;
+                default:
+                    printf("Invalid choice! Try again.\n");
+            }
+        } while (choice != 7);
     }
-
-    loadData(&students, &subjects, studentList, subjectName);
-
-    printf("            Welcome! Student Grade Calculation:\n");
-
-    setupSubjects(&subjects, subjectName);
-
-    do {
-        showMenu();
-        if (scanf("%d", &choice) != 1) break; 
-        getchar();
-
-        switch(choice) {
-            case 1:
-                addStudentData(&students, subjects, studentList, subjectName);
-                break;
-            case 2:
-                searchByName(students, studentList);
-                break;
-            case 3:
-                searchByRoll(students, studentList);
-                break;
-            case 4: 
-                showClassTopper(students, studentList);
-                break;
-            case 5:
-                printResultTable(students, subjects, studentList, subjectName);
-                break;
-            case 6:
-                deleteStudentData(&students, studentList);
-                break;
-            case 7:
-               saveData(students, subjects, studentList, subjectName);
-               printf("Exiting system...\n");
-               writeLog("Exited System");
-                break;
-            default:
-                printf("Invalid choice! Try again.\n");
-        }
-    } while (choice != 7);
-
+    
     return 0;
 }
